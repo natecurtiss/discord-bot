e88cef28ac18bb18634ee1a26b8e43c4c83fc9a1
natecurtiss <n8dev.cs@gmail.com>
2022-08-06 13:06:21 -0400

    Add ban, purge, and kick commands

--- DiscordBot/Source/Modules/Admin.cs
+++ DiscordBot/Source/Modules/Admin.cs
@@ -9,6 +9,11 @@ public class Admin : ModuleBase<SocketCommandContext>
     [RequireUserPermission(GuildPermission.Administrator)]
     public async Task Timeout(IGuildUser user, int minutes)
     {
+        if (minutes <= 0)
+        {
+            await ReplyAsync("Number of minutes must be positive!");
+            return;
+        }
         await user.SetTimeOutAsync(TimeSpan.FromMinutes(minutes));
         await Context.Message.ReplyAsync($"Timed out {user.Mention} for {minutes} minutes.");
     }
@@ -27,5 +32,35 @@ public class Admin : ModuleBase<SocketCommandContext>
     {
         await user.KickAsync();
         await Context.Message.ReplyAsync($"Kicked {user.Mention}");
+    }
+    
+    [Command("ban")]
+    [RequireUserPermission(GuildPermission.Administrator)]
+    public async Task Ban(IGuildUser user)
+    {
+        await user.BanAsync();
+        await Context.Message.ReplyAsync($"Banned {user.Mention}");
+    }
+    
+    [Command("purge")]
+    [RequireUserPermission(GuildPermission.Administrator)]
+    public async Task Purge(int amount)
+    {
+        if (amount <= 0)
+        {
+            await Context.Message.ReplyAsync("Number of messages must be positive!");
+            return;
+        }
+        var messages = await Context.Channel.GetMessagesAsync(Context.Message, Direction.Before, amount).FlattenAsync();
+        var filteredMessages = messages.Where(x => (DateTimeOffset.UtcNow - x.Timestamp).TotalDays <= 14).ToArray();
+        var count = filteredMessages.Length;
+        if (count == 0)
+        {
+            await Context.Message.ReplyAsync("Nothing to delete");
+            return;
+        }
+        foreach (var msg in filteredMessages) 
+            await Context.Channel.DeleteMessageAsync(msg);
+        await Context.Message.ReplyAsync($"Removed {count} {(count > 1 ? "messages" : "message")}");
     }
 }
