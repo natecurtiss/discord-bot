35e49d3ace6ec109c74b23c0a6f84b3920d73441
natecurtiss <n8dev.cs@gmail.com>
2022-08-06 11:13:20 -0400

    Remove redundant code

--- DiscordBot/Source/GlobalUsings.cs
+++
@@ -1,2 +0,0 @@
-﻿global using Discord;
-global using DiscordBot;
--- DiscordBot/Source/Program.cs
+++ DiscordBot/Source/Program.cs
@@ -1,1 +1,21 @@
-﻿new Bot().MainAsync().GetAwaiter().GetResult();
+﻿using Discord;
+using Discord.WebSocket;
+
+MainAsync().GetAwaiter().GetResult();
+
+async Task MainAsync()
+{
+    var client = new DiscordSocketClient();
+    client.Log += Log;
+
+    var token = Environment.GetEnvironmentVariable("TOKEN");
+    await client.LoginAsync(TokenType.Bot, token);
+    await client.StartAsync();
+    await Task.Delay(-1);
+}
+
+Task Log(LogMessage message)
+{
+    Console.WriteLine(message.ToString());
+    return Task.CompletedTask;
+}
